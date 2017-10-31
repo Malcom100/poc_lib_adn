d7c7f40fad49966ef6f57563f61dd8a9bf109720
Malcom <gaeltshilombo@gmail.com>
2017-10-31 11:03:43 +0100

    add a custom exception for the project

---
+++ design/src/main/java/com/gael/projects/design/models/exceptions/LibraryException.kt
@@ -0,0 +1,10 @@
+package com.gael.projects.design.models.exceptions
+
+import java.lang.RuntimeException
+
+/**
+ * Created on 31.10.17.
+ */
+
+class LibraryException(message : String) : RuntimeException(message) {
+}
