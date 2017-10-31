c21979c584ba6a4634c684f14f001e08d2e61cb5
Malcom <gaeltshilombo@gmail.com>
2017-10-31 13:12:22 +0100

    add a button in layout

--- app/src/main/java/com/gael/projects/libraryadn/login/LoginFragment.kt
+++ app/src/main/java/com/gael/projects/libraryadn/login/LoginFragment.kt
@@ -2,18 +2,21 @@ package com.gael.projects.libraryadn.login
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.gael.projects.libraryadn.R
+import com.gael.projects.libraryadn.dialogs.DialogPicture
+import com.gael.projects.libraryadn.interfaces.IPicture
+import com.gael.projects.libraryadn.utils.PictureUtils
 import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * Created by gael on 30.10.17.
  */
- class LoginFragment : Fragment(), LoginContract.View {
-
+ class LoginFragment : Fragment(), LoginContract.View, IPicture {
     lateinit var mPresenter : LoginContract.Presenter
 
     companion object {
@@ -45,6 +48,19 @@ import kotlinx.android.synthetic.main.fragment_login.*
         login_btn_login.setOnClickListener(View.OnClickListener { v ->
             mPresenter.login(login_lastname.text.toString(),login_lastname.text.toString(),login_email.text.toString(),login_number.text.toString(),"",activity.applicationContext)
         })
+
+        login_picture.setOnClickListener(View.OnClickListener { v ->
+            var dialog : DialogPicture = DialogPicture(activity,R.layout.dialog_picture,this)
+        })
     }
+
+    override fun takePhoto() {
+        Log.i("Test","take photo in call back");
+    }
+
+    override fun openGallery() {
+        activity.startActivityForResult(PictureUtils.intentGallery(), PictureUtils.REQUEST_GALLERY)
+    }
+
 
 }
